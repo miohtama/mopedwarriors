d5393c23c07e0d2a00160e13c86a71a9d3824e62
Mobile Joomla\! <mikko@kuneri.net>
2011-05-22 21:40:28 +0300

    Added first test level data

--- warrios/terrain.py
+++ warrios/terrain.py
@@ -1,22 +1,25 @@
-
+import pygame
 
 class Terrain(object):
     """
     2D map presenting the game arena.
+    
+    Default size is 824 x 768 (iPad screen optimized with one side panel)
     """
     
     def __init__(self):
         #: Raw pixel data of game mask 
         self.mask = None 
         
-        #: Raw pixel data of background layer 
+        #: Raw pixel data of background image layer
         self.background = None
         
-        #: Raw pixel data of foreground layer 
-        self.background = None
+        #: Raw pixel data of foreground layer   - can be destroyed
+        self.foreground = None
         
         
     def load(self, folder):
         """
         :param folder: Absolute path to folder from where to load fg, bg and mask files
         """
+        
